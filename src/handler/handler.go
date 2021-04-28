642eb312584a3ec3b899766fc3390ec1a9d116cb
peanuts <849421294godw@gmail.com>
2021-04-29 02:42:38 +0800

    complete three important handler

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -3,45 +3,126 @@ package handler
 import (
 	"net/http"
 
-	"framework/api/model"
+	"framework/api"
 	"framework/db"
 	"framework/logger"
 	"framework/net"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
+//SignIn 用户登录
 func SignIn(c *gin.Context) {
 	login := newLogin()
+	mongo := db.GetLastMongoClient()
+
 	err := c.ShouldBind(login)
 	if err != nil {
 		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
 		return
 	}
-	c.JSON(200, net.NewSuccessResponse(nil))
+	logger.Debug("mongo find account: %v", login.Account)
+	filter := bson.M{"account": login.Account}
+	user := &api.User{}
+	err = mongo.FindOne("user", user, filter)
+
+	if err != nil {
+		// user not found
+		logger.Info("mongo user not found. err:%v", err)
+		c.JSON(http.StatusOK, net.ResourceNotFoundResp)
+		return
+	}
+
+	if user.Password == api.EncryptBySha1(login.Password) {
+		token, err := api.InsertToken(user.UID)
+		if err != nil {
+			// token failed
+			logger.Error("insert user token failed")
+			c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+			return
+		}
+		// login success
+		c.JSON(http.StatusOK, net.NewSuccessResponse(gin.H{
+			"uid":   user.UID,
+			"token": token,
+		}))
+		return
+	}
+
+	logger.Info("user passwd not correct")
+	c.JSON(http.StatusOK, net.AuthFaildResp)
 }
 
 func SignOut(c *gin.Context) {
-	c.String(200, "this is logout post")
+	logout := newLogout()
+
+	err := c.ShouldBind(logout)
+	if err != nil {
+		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+		return
+	}
+
+	_, err = api.CheckToken(logout.Token)
+	if err != nil {
+		if db.IsNotExistError(err) {
+			c.JSON(http.StatusOK, net.TokenInvaildResp)
+			return
+		}
+		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+		return
+	}
+
+	err = api.DeleteToken(logout.Token)
+	if err != nil {
+		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, net.NewSuccessResponse(nil))
 }
 
+//SignUp 用户注册
 func SignUp(c *gin.Context) {
-	login := newLogin()
+	register := newRegister()
 	mongo := db.GetLastMongoClient()
-	// rds := db.GetLastRedisClient()
 
-	err := c.ShouldBind(login)
+	err := c.ShouldBind(register)
 	if err != nil {
 		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
 		return
 	}
-	user := model.NewUser(login.Account, login.Password)
-	res, err := mongo.InsertOne("user", user)
-	if err != nil {
-		logger.Error("mongo insert user err: %v", err)
-		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
-		return
+	// 先看是否重复
+	filter := bson.M{"account": register.Account}
+	user := &api.User{}
+	err = mongo.FindOne("user", user, filter)
+
+	if db.IsNoDocumentError(err) {
+		user := api.NewUser(register.Account, api.EncryptBySha1(register.Password))
+		res, err := mongo.InsertOne("user", user)
+		if err != nil {
+			logger.Error("mongo insert user err: %v", err)
+			c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+			return
+		}
+		logger.Debug("mongo insert user success", res.InsertedID)
+
+		// 插入token
+		token, err := api.InsertToken(user.UID)
+		if err != nil {
+			logger.Error("redis insert user token err: %v", err)
+			c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+			return
+		}
+
+		c.JSON(200, net.NewSuccessResponse(gin.H{
+			"uid":   user.UID,
+			"token": token,
+		}))
+
+	} else {
+		logger.Info("mongo user already exists or err: %v", err)
+		c.JSON(http.StatusOK, net.ResourceExistsResp)
 	}
-	logger.Debug("mongo insert user success", res.InsertedID)
-	c.JSON(200, net.NewSuccessResponse(nil))
+
 }
--- src/handler/model.go
+++ src/handler/model.go
@@ -5,6 +5,24 @@ type login struct {
 	Password string `json:"password"`
 }
 
+type logout struct {
+	UID   string `json:"uid"`
+	Token string `json:"token"`
+}
+
+type register struct {
+	Account  string `json:"account"`
+	Password string `json:"password"`
+}
+
 func newLogin() *login {
 	return &login{}
+}
+
+func newLogout() *logout {
+	return &logout{}
+}
+
+func newRegister() *register {
+	return &register{}
 }
