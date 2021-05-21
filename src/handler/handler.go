835dac367e103d33d8533fd54f146a5306edd84d
peanuts <849421294godw@gmail.com>
2021-05-22 01:59:57 +0800

    add encoding package

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -2,14 +2,14 @@ package handler
 
 import (
 	"fmt"
+	"framework/api/model"
+	"framework/encoding"
 	"net/http"
 
 	"framework/api"
 	"framework/cfgargs"
 	"framework/db"
 	"framework/logger"
-	"framework/net"
-
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -21,31 +21,31 @@ func SignIn(c *gin.Context) {
 
 	err := c.ShouldBind(login)
 	if err != nil {
-		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 		return
 	}
 	logger.Debug("mongo find account: %v", login.Account)
 	filter := bson.M{"account": login.Account}
-	user := &api.User{}
+	user := &model.User{}
 	err = mongo.FindOne("User", user, filter)
 
 	if err != nil {
 		// user not found
 		logger.Info("mongo user not found. err:%v", err)
-		c.JSON(http.StatusOK, net.ResourceNotFoundResp)
+		c.JSON(http.StatusOK, api.ResourceNotFoundResp)
 		return
 	}
 
-	if user.Password == api.EncryptBySha1(fmt.Sprintf("%v%v", login.Password, cfgargs.GetLastSrvConfig().AppKey)) {
+	if user.Password == encoding.EncryptBySha1(fmt.Sprintf("%v%v", login.Password, cfgargs.GetLastSrvConfig().AppKey)) {
 		token, err := api.InsertToken(user.UID)
 		if err != nil {
 			// token failed
 			logger.Error("insert user token failed")
-			c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+			c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 			return
 		}
 		// login success
-		c.JSON(http.StatusOK, net.NewSuccessResponse(gin.H{
+		c.JSON(http.StatusOK, api.NewSuccessResponse(gin.H{
 			"uid":   user.UID,
 			"token": token,
 		}))
@@ -53,7 +53,7 @@ func SignIn(c *gin.Context) {
 	}
 
 	logger.Info("user passwd not correct")
-	c.JSON(http.StatusOK, net.AuthFaildResp)
+	c.JSON(http.StatusOK, api.AuthFaildResp)
 }
 
 func SignOut(c *gin.Context) {
@@ -61,27 +61,27 @@ func SignOut(c *gin.Context) {
 
 	err := c.ShouldBind(logout)
 	if err != nil {
-		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 		return
 	}
 
 	_, err = api.CheckToken(logout.Token)
 	if err != nil {
 		if db.IsNotExistError(err) {
-			c.JSON(http.StatusOK, net.TokenInvaildResp)
+			c.JSON(http.StatusOK, api.TokenInvaildResp)
 			return
 		}
-		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 		return
 	}
 
 	err = api.DeleteToken(logout.Token)
 	if err != nil {
-		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 		return
 	}
 
-	c.JSON(http.StatusOK, net.NewSuccessResponse(nil))
+	c.JSON(http.StatusOK, api.NewSuccessResponse(nil))
 }
 
 //SignUp 用户注册
@@ -91,20 +91,20 @@ func SignUp(c *gin.Context) {
 
 	err := c.ShouldBind(register)
 	if err != nil {
-		c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 		return
 	}
 	// 先看是否重复
 	filter := bson.M{"account": register.Account}
-	user := &api.User{}
+	user := &model.User{}
 	err = mongo.FindOne("User", user, filter)
 
 	if db.IsNoDocumentError(err) {
-		user := api.NewUser(register.Account, api.EncryptBySha1(fmt.Sprintf("%v%v", register.Password, cfgargs.GetLastSrvConfig().AppKey)))
+		user := model.NewUser(register.Account, encoding.EncryptBySha1(fmt.Sprintf("%v%v", register.Password, cfgargs.GetLastSrvConfig().AppKey)))
 		res, err := mongo.InsertOne("User", user)
 		if err != nil {
 			logger.Error("mongo insert user err: %v", err)
-			c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+			c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 			return
 		}
 		logger.Debug("mongo insert user success", res.InsertedID)
@@ -113,18 +113,18 @@ func SignUp(c *gin.Context) {
 		token, err := api.InsertToken(user.UID)
 		if err != nil {
 			logger.Error("redis insert user token err: %v", err)
-			c.JSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+			c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 			return
 		}
 
-		c.JSON(200, net.NewSuccessResponse(gin.H{
+		c.JSON(200, api.NewSuccessResponse(gin.H{
 			"uid":   user.UID,
 			"token": token,
 		}))
 
 	} else {
 		logger.Info("mongo user already exists or err: %v", err)
-		c.JSON(http.StatusOK, net.ResourceExistsResp)
+		c.JSON(http.StatusOK, api.ResourceExistsResp)
 	}
 
 }
